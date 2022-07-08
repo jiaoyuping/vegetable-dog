cc7c246d68ce2acc12db86b063814d1c82755ecf
jiaoyuping <108911873+jiaoyuping@users.noreply.github.com>
2022-07-08 12:54:35 +0800

    Add files via upload

---
+++ test_01.c
@@ -0,0 +1,89 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+
+
+
+//int main()
+//{
+//	int a, b, c;//       int 整数；float 小数
+//	a = 112;
+//	b = 556;
+//	c = a + b;
+//	printf("%d\n", c);
+//	system("pause");
+//	return 0;
+//}
+//int main( )
+//{
+//	printf("\a\a\a");//“叮咚”一响
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+////数组概念
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+
+////数组长度
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(a));
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+//++在前和++在后的区别
+//int main()
+//{
+//	int a = 1;
+//	int b = (++a) + (++a) + (++a);
+//	//int b = a++;
+//	printf("%d\n", b);
+//	printf("%d\n", a);
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;//用p指向a的地址，p任意不唯一，狭义的说是代替
+//	*p = 20;
+//	printf("%d\n", a);
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+//printf函数返回整形位数
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", printf("%d", 98))));
+//	printf("\n");
+//}
